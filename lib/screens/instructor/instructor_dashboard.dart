be9895b081658a2a02a7990b417dc48b033b3c9d
Hamza <hamzali4642@gmail.com>
2023-09-01 11:40:06 +0500

    instructor registration

---
+++ lib/screens/instructor/instructor_dashboard.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InstructorDashboard extends StatelessWidget {
+  const InstructorDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/widgets/custom_text.dart
+++ lib/widgets/custom_text.dart
@@ -1,21 +1,32 @@
-
 import 'package:flutter/material.dart';
 
 import '../extras/app_textstyles.dart';
 import '../extras/colors.dart';
 
 class CustomText extends StatelessWidget {
-  CustomText({super.key,required this.text,this.fontSize,this.fontWeight,this.textColor});
-String text;
-double? fontSize;
-FontWeight? fontWeight;
-Color? textColor;
+  CustomText(
+      {super.key,
+      required this.text,
+      this.fontSize,
+      this.fontWeight,
+      this.textColor});
+
+  String text;
+  double? fontSize;
+  FontWeight? fontWeight;
+  Color? textColor;
+
   @override
   Widget build(BuildContext context) {
-    return Text(text,style: AppTextStyles.poppins(
+    return Text(
+      text,
+      style: AppTextStyles.poppins(
         style: TextStyle(
-            fontWeight: fontWeight,
-            fontSize: fontSize,
-            color: textColor)),);
+          fontWeight: fontWeight,
+          fontSize: fontSize,
+          color: textColor,
+        ),
+      ),
+    );
   }
 }
