bdd0fecd2a53083d1f22e2b0c228a1788e8baf7d
M Ahmed <razaraza3536@gmail.com>
2023-09-12 15:41:15 +0500

    notifications

--- lib/screens/dashboard/notification_screen.dart
+++ lib/screens/dashboard/notification_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:projeto/extras/app_assets.dart';
 import 'package:projeto/extras/app_textstyles.dart';
 import 'package:projeto/extras/colors.dart';
+import 'package:projeto/provider/data_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:utility_extensions/utility_extensions.dart';
 import 'package:projeto/widgets/button_widget.dart';
 import 'package:projeto/widgets/custom_asset_image.dart';
@@ -11,8 +13,10 @@ import 'package:projeto/widgets/margin_widget.dart';
 import '../../widgets/c_profile_app_bar.dart';
 
 class NotificationScreen extends StatefulWidget {
-   NotificationScreen({super.key,this.isInstructor});
-bool? isInstructor;
+  NotificationScreen({super.key, this.isInstructor});
+
+  bool? isInstructor;
+
   @override
   State<NotificationScreen> createState() => _NotificationScreenState();
 }
@@ -20,41 +24,57 @@ bool? isInstructor;
 class _NotificationScreenState extends State<NotificationScreen> {
   late double width, padding;
 
+  late DataProvider dataProvider;
+
   @override
   Widget build(BuildContext context) {
     width = context.width;
     padding = width * 0.04;
 
-    return Scaffold(
-      appBar: CustomAppBar("Notificações",isInstructor: widget.isInstructor),
-      body: Padding(
-        padding: EdgeInsets.only(left: padding, right: padding),
-        child: SingleChildScrollView(
+    return Consumer<DataProvider>(builder: (context, value, child) {
+      dataProvider = value;
+
+      return Scaffold(
+        appBar: CustomAppBar("Notificações", isInstructor: widget.isInstructor),
+        body: Padding(
+          padding: EdgeInsets.only(left: padding, right: padding),
           child: Column(
             children: [
-              notificationRow(AppIcons.message),
-              const DividerWidget(),
-              notificationRow(AppIcons.message),
-              const DividerWidget(),
-              notificationRow(AppIcons.message),
-              const DividerWidget(),
-              notificationRow(AppIcons.setting),
-              const DividerWidget(),
-              notificationRow(AppIcons.approval),
-              const DividerWidget(),
-              InkWell(
-                onTap: (){
-                  showDialog(context: context, builder: (BuildContext context){
-                    return requestsLessonDialogue(context);
-                  });
-                },
-                child: notificationRow(AppIcons.calendar),
-              ),
-              const DividerWidget(),
+              displayNotifications(),
+              // notificationRow(AppIcons.message),
+              // const DividerWidget(),
+              // notificationRow(AppIcons.message),
+              // const DividerWidget(),
+              // notificationRow(AppIcons.message),
+              // const DividerWidget(),
+              // notificationRow(AppIcons.setting),
+              // const DividerWidget(),
+              // notificationRow(AppIcons.approval),
+              // const DividerWidget(),
+              // InkWell(
+              //   onTap: () {
+              //     showDialog(
+              //         context: context,
+              //         builder: (BuildContext context) {
+              //           return requestsLessonDialogue(context);
+              //         });
+              //   },
+              //   child: notificationRow(AppIcons.calendar),
+              // ),
+              // const DividerWidget(),
             ],
           ),
         ),
-      ),
+      );
+    });
+  }
+
+  Widget displayNotifications() {
+
+    return Expanded(
+      child: ListView.builder(itemBuilder: (ctx, index) {
+        return notificationRow(AppIcons.message);
+      }),
     );
   }
 
@@ -94,7 +114,8 @@ class _NotificationScreenState extends State<NotificationScreen> {
       ],
     );
   }
-  Widget requestsLessonDialogue(BuildContext context){
+
+  Widget requestsLessonDialogue(BuildContext context) {
     return AlertDialog(
       content: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -102,27 +123,58 @@ class _NotificationScreenState extends State<NotificationScreen> {
         children: [
           Align(
             alignment: Alignment.topRight,
-            child: IconButton(onPressed:(){
-              context.pop();
-            }, icon: const Icon(Icons.close)),
+            child: IconButton(
+                onPressed: () {
+                  context.pop();
+                },
+                icon: const Icon(Icons.close)),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Claudia Silva solicita aulas",style: AppTextStyles.subTitleMedium(),),
+              Text(
+                "Claudia Silva solicita aulas",
+                style: AppTextStyles.subTitleMedium(),
+              ),
             ],
           ),
           const MarginWidget(),
-          Text("Localização: Lorem Ipsum",style: AppTextStyles.subTitleRegular(),),
-          Text("Data: 17-06-2023",style: AppTextStyles.subTitleRegular(),),
-          Text("Horário: 10h00",style: AppTextStyles.subTitleRegular(),),
-          Text("Quantidade de aulas: 1",style: AppTextStyles.subTitleRegular(),),
-          const MarginWidget(factor: 2,),
-          Row(children: [
-            Expanded(child: TextButton(onPressed:(){}, child:Text("Rejeitar",style: AppTextStyles.captionMedium(size: 12,color: CColors.primary),))),
-            const MarginWidget(isHorizontal: true,factor: 2,),
-            Expanded(child: ButtonWidget(name: "Aceitar", onPressed:(){}))
-          ],)
+          Text(
+            "Localização: Lorem Ipsum",
+            style: AppTextStyles.subTitleRegular(),
+          ),
+          Text(
+            "Data: 17-06-2023",
+            style: AppTextStyles.subTitleRegular(),
+          ),
+          Text(
+            "Horário: 10h00",
+            style: AppTextStyles.subTitleRegular(),
+          ),
+          Text(
+            "Quantidade de aulas: 1",
+            style: AppTextStyles.subTitleRegular(),
+          ),
+          const MarginWidget(
+            factor: 2,
+          ),
+          Row(
+            children: [
+              Expanded(
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Rejeitar",
+                        style: AppTextStyles.captionMedium(
+                            size: 12, color: CColors.primary),
+                      ))),
+              const MarginWidget(
+                isHorizontal: true,
+                factor: 2,
+              ),
+              Expanded(child: ButtonWidget(name: "Aceitar", onPressed: () {}))
+            ],
+          )
         ],
       ),
     );
